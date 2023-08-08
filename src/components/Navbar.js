a3c66c6a12c2d5ca3d6ca732f783429aca84cbee
Pick-Y <a.scarcella.as@gmail.com>
2023-08-08 14:15:52 +1000

    created Navbar with menu items

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import styled  from "styled-components"
+import netflixLogo from '../assets/netflixLogo.png';
+
+
+
+const NavContainer = styled.div`
+
+ color: white;
+ 
+
+`
+
+const Logo = styled.img`
+
+width: 130px;
+height: 50px;
+
+`
+
+const Ul = styled.ul`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+`
+const Li = styled.li`
+list-style: none;
+margin-right: 2rem;
+color: white;
+`
+
+const  Navbar = () => {
+
+
+    return (        
+
+        <>
+        
+           
+            
+            <Ul>
+                <Li><Logo src={netflixLogo} alt="logo"></Logo></Li>
+                <Li>Home</Li>
+                <Li>TV Shows</Li>
+                <Li>Movies</Li>
+                <Li>New & popular</Li>
+                <Li>My list</Li>
+                <Li>Browse by Languages</Li>
+
+
+            </Ul>
+            
+       
+    
+
+
+        </>
+    )
+}
+
+export default Navbar;
